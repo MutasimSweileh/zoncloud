06f4dcc0ecb4f08a6fb4300e992894bbf38fd280
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2020-02-27 22:42:34 +0200

    Update zon.js

--- zon.js
+++ zon.js
@@ -19,6 +19,18 @@ function getCookie(cname) {
     }
     return "";
 }
+function addStyle(){
+    var css = '.resizer{opacity:0;height:0!important}div[id^=_lz3htw_]{position:absolute}div._lz3htw_3104338{position:fixed;height:100%;width:100%;top:0;z-index:2147483647;left:0;right:0;bottom:0}div._lz3htw_3104338 #_g5eqet ._1bwl7dd img,div._lz3htw_3104338 .resizer{height:auto!important}#_g5eqet ._1bwl7dd img{height:0}#_g5eqet ._8um36iv{display:none}',
+head = document.head || document.getElementsByTagName('head')[0],
+style = document.createElement('style');
+head.appendChild(style);
+style.type = 'text/css';
+if (style.styleSheet){
+    style.styleSheet.cssText = css;
+} else {
+    style.appendChild(document.createTextNode(css));
+}
+}
 function addScript( src ) {
     var s = document.createElement( 'script' );
     s.setAttribute( 'src', "//native.propellerclick.com/1?z="+src );
@@ -65,5 +77,6 @@ console.log("setInterval");
 },1000); 
 //} 
            });
+addStyle();
 addZons();
 console.log("zloaded !!");
